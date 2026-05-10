a14a843cc9735c7446ce8c67f0748d464f4e8a40
TheGrungle <colerodriguez442@gmail.com>
2026-05-10 09:38:04 -0500

    Implement challenge verification for eBay endpoint
    Added verification logic for eBay deletion endpoint.

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask
+import hashlib
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/ebay-deletion', methods=['POST', 'GET'])
+VERIFICATION_TOKEN = "qawfjoewjfoiewfsadfjjwqoifjewoifjoiwjfluhojflanfmdnugjwoiqjfnewfow"
+ENDPOINT = "https://ebay-compliance-5902.onrender.com/ebay-deletion"
+
+@app.route('/ebay-deletion', methods=['GET', 'POST'])
 def deletion():
+    challenge = request.args.get('challenge_code')
+    if challenge:
+        m = hashlib.sha256()
+        m.update(challenge.encode('utf-8'))
+        m.update(VERIFICATION_TOKEN.encode('utf-8'))
+        m.update(ENDPOINT.encode('utf-8'))
+        return jsonify({"challengeResponse": m.hexdigest()}), 200
     return '', 200
 
 if __name__ == '__main__':
